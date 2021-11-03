0226d3e92f64e445131fffdb7991144c01d8e7db
blakkur8 <sigmarsnaer@gmail.com>
2021-11-03 22:53:03 +0000

    breytti buyermodel

--- EmailService/src/application.py
+++ EmailService/src/application.py
@@ -1,42 +1,63 @@
-import yagmail
-import pika
+from EmailService.src.email_model import EmailModel
+import pika, sys, os
+import time
 from retry import retry
-from emaill import send_email
+
+from emaill import EmailSender
+from email_model import EmailModel
 
 
 @retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
 def get_connection():
-    # creating rabbitmq connection
-    return pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    # TODO: create rabbitmq connection
+    return pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+
+
+def create_email_model(body):
+    return EmailModel(
+        email_to=body.email_to,
+        subject=body.subject,
+        content=body.content,
+    )
+
+
+user = "honnunproject@gmail.com"
+password = "charliesheen14"
+
+email_sender = EmailSender(user, password)
 
 
 def main():
+    print("getting connection")
     connection = get_connection()
+    print("got connection")
     channel = connection.channel()
-
-    channel.exchange_declare(
-        exchange="order-created", exchange_type="direct", durable=True
-    )
 
     channel.queue_declare(queue="order_created_email_queue", durable=True)
 
-    channel.queue_bind(
-        exchange="order-created",
-        queue="order_created_email_queue",
-        routing_key="order_created_email_queue",
-    )
-
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
-        time.sleep(5)
+        body = body.decode()  # Body hefur "email_to": "", "subject": "", "content": ""
+        email = create_email_model(body)
+        email_sender.send_email(email)
         print(" [x] Done")
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
+    channel.basic_qos(prefetch_count=1)
     channel.basic_consume(
-        queue=email_queue, on_message_callback=callback, auto_ack=True
+        queue="order_created_email_queue", on_message_callback=callback
     )
+
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
